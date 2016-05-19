cef945e617d191c8ad552362fff8f34195d08979
yaches <slavik-kruglov@mail.ru>
2016-05-19 23:28:19 +0300

    5-й вариант
    Приоритет по правилу левой руки

--- Runner.cpp
+++ Runner.cpp
@@ -27,68 +27,116 @@ Direction Runner::step()
 		return Direction::RIGHT;
 	}
 
+	if (map_size > 1)
+	{
+		setPriority();
+	}
+
 	photo();
 
 	int x_ind = position.x - min_crd;
 	int y_ind = position.y - min_crd;
 
-	Point up_point = map[y_ind + 1][x_ind];
-	Point down_point = map[y_ind - 1][x_ind];
-	Point right_point = map[y_ind][x_ind + 1];
-	Point left_point = map[y_ind][x_ind - 1];
+	Point up_point = lab_map[y_ind + 1][x_ind];
+	Point down_point = lab_map[y_ind - 1][x_ind];
+	Point right_point = lab_map[y_ind][x_ind + 1];
+	Point left_point = lab_map[y_ind][x_ind - 1];
 
-	if (!right_point.wall and !right_point.was_here)
+	for (int i = 0; i < 4; ++i)
 	{
-		position = map[y_ind][x_ind + 1];
-		return Direction::RIGHT;
-	}
-	if (!down_point.wall and !down_point.was_here)
-	{
-		position = map[y_ind - 1][x_ind];
-		return Direction::DOWN;
-	}
-	if (!left_point.wall and !left_point.was_here)
-	{
-		position = map[y_ind][x_ind - 1];
-		return Direction::LEFT;
-	}
-	if (!up_point.wall and !up_point.was_here)
-	{
-		position = map[y_ind + 1][x_ind];
-		return Direction::UP;
+		switch(priority[i])
+		{
+			case Direction::RIGHT:
+				if (!right_point.wall and !right_point.was_here)
+				{
+					position = lab_map[y_ind][x_ind + 1];
+					prev_step = Direction::RIGHT;
+					++cnt[Direction::RIGHT];
+					return Direction::RIGHT;
+				}
+				break;
+			case Direction::DOWN:
+				if (!down_point.wall and !down_point.was_here)
+				{
+					position = lab_map[y_ind - 1][x_ind];
+					prev_step = Direction::DOWN;
+					++cnt[Direction::DOWN];
+					return Direction::DOWN;
+				}
+				break;
+			case Direction::LEFT:
+				if (!left_point.wall and !left_point.was_here)
+				{
+					position = lab_map[y_ind][x_ind - 1];
+					prev_step = Direction::LEFT;
+					++cnt[Direction::LEFT];
+					return Direction::LEFT;
+				}
+				break;
+			case Direction::UP:
+				if (!up_point.wall and !up_point.was_here)
+				{
+					position = lab_map[y_ind + 1][x_ind];
+					prev_step = Direction::UP;
+					++cnt[Direction::UP];
+					return Direction::UP;
+				}
+				break;
+		}
 	}
 
-	if (!right_point.wall)
+	for (int i = 0; i < 4; ++i)
 	{
-		position = map[y_ind][x_ind + 1];
-		return Direction::RIGHT;
+		switch(priority[i])
+		{
+			case Direction::RIGHT:
+				if (!right_point.wall)
+				{
+					position = lab_map[y_ind][x_ind + 1];
+					prev_step = Direction::RIGHT;
+					++cnt[Direction::RIGHT];
+					return Direction::RIGHT;
+				}
+				break;
+			case Direction::DOWN:
+				if (!down_point.wall)
+				{
+					position = lab_map[y_ind - 1][x_ind];
+					prev_step = Direction::DOWN;
+					++cnt[Direction::DOWN];
+					return Direction::DOWN;
+				}
+				break;
+			case Direction::LEFT:
+				if (!left_point.wall)
+				{
+					position = lab_map[y_ind][x_ind - 1];
+					prev_step = Direction::LEFT;
+					++cnt[Direction::LEFT];
+					return Direction::LEFT;
+				}
+				break;
+			case Direction::UP:
+				if (!up_point.wall)
+				{
+					position = lab_map[y_ind + 1][x_ind];
+					prev_step = Direction::UP;
+					++cnt[Direction::UP];
+					return Direction::UP;
+				}
+				break;
+		}
 	}
-	if (!down_point.wall)
-	{
-		position = map[y_ind - 1][x_ind];
-		return Direction::DOWN;
-	}
-	if (!left_point.wall)
-	{
-		position = map[y_ind][x_ind - 1];
-		return Direction::LEFT;
-	}
-	if (!up_point.wall)
-	{
-		position = map[y_ind + 1][x_ind];
-		return Direction::UP;
-	}
-
 }
 
 void Runner::photo()
 {
-	if (map == NULL)
+	if (lab_map == NULL)
 	{
-		map = new Point*[map_size];
+		lab_map = new Point*[map_size];
 		for (int i = 0; i < map_size; ++i)
 		{
-			map[i] = new Point[map_size];
+			lab_map[i] = new Point[map_size];
 		}
 	}
 
@@ -100,26 +148,26 @@ void Runner::photo()
 	int x_ind = position.x - min_crd;
 	int y_ind = position.y - min_crd;
 
-	map[y_ind][x_ind + 1].fog = 0;
-	map[y_ind][x_ind - 1].fog = 0;
-	map[y_ind + 1][x_ind].fog = 0;
-	map[y_ind - 1][x_ind].fog = 0;
+	lab_map[y_ind][x_ind + 1].fog = 0;
+	lab_map[y_ind][x_ind - 1].fog = 0;
+	lab_map[y_ind + 1][x_ind].fog = 0;
+	lab_map[y_ind - 1][x_ind].fog = 0;
 
 	if (current_status.up == BlockType::WALL)
 	{
-		map[y_ind + 1][x_ind].wall = 1;
+		lab_map[y_ind + 1][x_ind].wall = 1;
 	}
 	if (current_status.down == BlockType::WALL)
 	{
-		map[y_ind - 1][x_ind].wall = 1;
+		lab_map[y_ind - 1][x_ind].wall = 1;
 	}
 	if (current_status.left == BlockType::WALL)
 	{
-		map[y_ind][x_ind - 1].wall = 1;
+		lab_map[y_ind][x_ind - 1].wall = 1;
 	}
 	if (current_status.right == BlockType::WALL)
 	{
-		map[y_ind][x_ind + 1].wall = 1;
+		lab_map[y_ind][x_ind + 1].wall = 1;
 	}
 
 	if (position.was_here)
@@ -130,8 +178,8 @@ void Runner::photo()
 	position.was_here = 1;
 	position.fog = 0;
 
-	int walls = map[y_ind + 1][x_ind].wall + map[y_ind - 1][x_ind].wall \
-		+ map[y_ind][x_ind - 1].wall + map[y_ind][x_ind + 1].wall;
+	int walls = lab_map[y_ind + 1][x_ind].wall + lab_map[y_ind - 1][x_ind].wall \
+		+ lab_map[y_ind][x_ind - 1].wall + lab_map[y_ind][x_ind + 1].wall;
 
 	if (walls < 2)
 	{
@@ -149,7 +197,7 @@ void Runner::photo()
 		position.wall = 1;
 	}
 
-	map[y_ind][x_ind] = position;
+	lab_map[y_ind][x_ind] = position;
 
 }
 
@@ -174,17 +222,17 @@ void Runner::resize_map()
 	{
 		for (int j = 0; j < map_size; ++j)
 		{
-			new_map[i + map_size][j + map_size] = map[i][j];
+			new_map[i + map_size][j + map_size] = lab_map[i][j];
 		}
 	}
 
 	for (int i = 0; i < map_size; ++i)
 	{
-		delete[] map[i];
+		delete[] lab_map[i];
 	}
-	delete[] map;
+	delete[] lab_map;
 
-	map = new_map;
+	lab_map = new_map;
 	min_crd = new_min;
 	max_crd = new_max;
 	map_size = new_size;
@@ -198,7 +246,7 @@ void Runner::draw()
 		{
 			// cout << '(' << map[i][j].x << ", " << map[i][j].y << ") ";
 			// cout << map[i][j].wall << ' ';
-			cout << map[map_size - 1 - i][j].fog << ' ';
+			cout << lab_map[map_size - 1 - i][j].fog << ' ';
 		}
 		cout << endl;
 	}
@@ -209,7 +257,78 @@ void Runner::freeMap()
 {
 	for (int i = 0; i < map_size; ++i)
 	{
-		delete[] map[i];
+		delete[] lab_map[i];
 	}
-	delete[] map;
+	delete[] lab_map;
+}
+
+void Runner::setPriority()
+{
+	// USE LEFT-HAND RULE
+
+	priority[0] = clockwise(prev_step, 0);
+	for (int i = 1; i < 4; ++i)
+	{
+		priority[i] = clockwise(priority[i - 1], 1);
+	}
+
+	// map<Direction, int>::iterator it;
+	// int i = 0;
+	// pair<Direction, int> cnt_array[4];
+
+	// for (it = cnt.begin(); it != cnt.end(); ++it)
+	// {
+	// 	cnt_array[i++] = *it;
+	// }
+
+	// pair<Direction, int> tmp;
+
+	// for (i = 0; i < 4; ++i)
+	// {
+	// 	for (int j = i + 1; j < 4; ++j)
+	// 	{
+	// 		if (cnt_array[j].second > cnt_array[i].second)
+	// 		{
+	// 			tmp = cnt_array[i];
+	// 			cnt_array[i] = cnt_array[j];
+	// 			cnt_array[j] = tmp;
+	// 		}
+	// 	}
+	// }
+
+	// for (i = 0; i < 4; ++i)
+	// {
+	// 	priority[i] = cnt_array[i].first;
+	// }
+}
+
+Direction Runner::clockwise(Direction step, bool way)
+{
+	switch (step)
+	{
+		case Direction::RIGHT:
+			if (way)
+				return Direction::DOWN;
+			else
+				return Direction::UP;
+			break;
+		case Direction::DOWN:
+			if (way)
+				return Direction::LEFT;
+			else
+				return Direction::RIGHT;
+			break;
+		case Direction::LEFT:
+			if (way)
+				return Direction::UP;
+			else
+				return Direction::DOWN;
+			break;
+		case Direction::UP:
+			if (way)
+				return Direction::RIGHT;
+			else
+				return Direction::LEFT;
+			break;
+	}
 }
