ffa686e8c0e23e868500490c7ba13e22f0cf7a03
yaches <slavik-kruglov@mail.ru>
2016-05-21 05:32:50 +0300

    Var 16
    DURL

--- Runner.hpp
+++ Runner.hpp
@@ -49,7 +49,7 @@ class Runner: public RunnerBase {
         int max_crd = 0;
         Point** lab_map = NULL;
         Point position {0, 0, 1, 0, 0, 0, 0};
-        Direction priority[4] = {Direction::DOWN, Direction::RIGHT, Direction::LEFT, Direction::UP};
+        Direction priority[4] = {Direction::DOWN, Direction::UP, Direction::RIGHT, Direction::LEFT};
         bool go = 0;
 };
 
