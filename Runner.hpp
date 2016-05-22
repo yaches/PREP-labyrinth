f5d943c4e530c13974e768e64baa4d92cd7ce9fd
yaches <slavik-kruglov@mail.ru>
2016-05-22 22:58:43 +0300

    Add files via upload

--- Runner.hpp
+++ Runner.hpp
@@ -7,6 +7,8 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <map>
+#include <random>
 
 #include "RunnerBase.hpp"
 
@@ -14,29 +16,48 @@ using namespace std;
 
 class Runner: public RunnerBase {
     public:
+        struct Point
+        {
+            int x;
+            int y;
+            bool fog;
+            bool was_here;
+            bool was_here_again;
+            bool crossway;
+            bool wall;
+            Direction step;
+        };
+
         Direction step();
         void photo();
         void resize_map();
         void draw();
         void freeMap();
+        void setPriority();
+        Direction clockwise(Direction, bool);
+        Point getPoint(Direction);
+        void printStep(Direction);
+        void savePosition(Point);
 
-        struct Point
-        {
-        	int x;
-        	int y;
-        	bool fog;
-            bool was_here;
-            bool was_here_again;
-            bool crossway;
-            bool wall;
-        };
+        Direction prev_step;
+
+        map<Direction, int> cnt = { {Direction::UP, 0}, {Direction::RIGHT, 0}, 
+        {Direction::DOWN, 0}, {Direction::LEFT, 0} };
+
+        int steps = 0;
+        int crosses = 0;
 
         int map_size = 1;
         int min_crd = 0;
         int max_crd = 0;
-        Point** map = NULL;
+        Point** lab_map = NULL;
         Point position {0, 0, 1, 0, 0, 0, 0};
+        Direction priority[4] = {Direction::RIGHT, Direction::LEFT, Direction::DOWN, Direction::UP};
+        bool go = 0;
 };
 
+// RLDU 190
+// RDUL 198
+// RDLU 206
 
 #endif //LABYRINTH_RUNNER_HPP
